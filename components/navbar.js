c2700f6a1bcc69845dccd1fb874d23c1cd37369d
Pablo Prado <pablo21prad@gmail.com>
2025-11-19 21:17:30 -0300

    8

--- components/navbar.js
+++ components/navbar.js
@@ -10,7 +10,7 @@ const navElements = [
 export const navbar =  `
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">CarMotors</a>
+        <a class="navbar-brand" href="/pages/carmotors/carmotors.html">CarMotors</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
             data-bs-target="#navbarNav" aria-controls="navbarNav" 
             aria-expanded="false" aria-label="Toggle navigation">
--- index.js
+++ index.js
@@ -5,3 +5,24 @@ import { navbar } from './components/navbar.js';
 let navcontainer = document.querySelector(`header`);
 window.addEventListener(`load`, () => {
     navcontainer.innerHTML = navbar})
+
+const form = document.getElementById("Login");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault(); 
+    
+    let userEmail = document.getElementById("email").value;
+    let userPass = document.getElementById("pass").value;
+fetch("/data/usuarios.json")
+    .then(res => res.json())
+    .then(usuario => {
+        const usuario = usuarios.find(e=> e.email === userEmail && e.password === userPass);
+        if (usuario) {
+            sessionStorage.setItem("userdata", JSON.stringify(usuario));
+            window.location.href = "/pages/home/home.html";
+        } else {
+            alert("Credenciales inválidas. Por favor, inténtalo de nuevo.");
+        }
+    });
+
+})
